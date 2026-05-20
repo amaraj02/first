04de6bbd16576dacd860826f5c81a35d7a1a0d17
amaraj02 <braj30173@gmail.com>
2026-05-20 15:24:22 +0545

    Add files via upload

---
+++ class first.java
@@ -0,0 +1,18 @@
+ class First 
+ {
+    public void Greeting (){
+
+        System.out.println("Hello.");
+    }
+}
+
+ class Main {
+    public static void main (String[] args){
+
+        First f = new First();
+        f.Greeting();
+}
+
+
+    }
+    
