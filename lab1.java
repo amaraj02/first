0d5ae4e49d0ec033b20c92f8ca88b9790e44a02a
amaraj02 <braj30173@gmail.com>
2026-05-24 09:36:06 +0545

    Add files via upload

--- lab1.java
+++ lab1.java
@@ -1,11 +1,37 @@
-import java.util.Scanner;
-public class lab1 {
+ import java.util.Scanner;
+
+ class Userinput {
+
+    int roll;
+    String name;
+
+    Userinput(int r, String n) {
+        roll = r;
+        name = n;
+    }
+
+    void display() {
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll: " + roll);
+    }
+        
+
+
+
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = sc.nextInt();
-        int result = number ;
-        System.out.println("The result is: " + result);
-        sc.close();
+
+        System.out.println("Enter your name:");
+        String n = sc.nextLine();
+
+        System.out.println("Enter your roll:");
+        int r = sc.nextInt();
+
+        Userinput u = new Userinput(r, n);
+
+        u.display();
     }
 }
